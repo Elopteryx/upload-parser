5c13b2948a9b83aa7562a5d592743c8b34e66677
Adam Forgacs <adamforgacs256@gmail.com>
2015-03-15 21:52:06 +0100

    Checkstyle fixes for the new tests.

---
+++ core/src/test/java/com/elopteryx/paint/upload/impl/AsyncUploadParserTest.java
@@ -0,0 +1,22 @@
+package com.elopteryx.paint.upload.impl;
+
+import static com.elopteryx.paint.upload.util.Servlets.newRequest;
+import static org.mockito.Mockito.when;
+
+import com.elopteryx.paint.upload.UploadParser;
+
+import org.junit.Test;
+
+import javax.servlet.http.HttpServletRequest;
+
+public class AsyncUploadParserTest {
+
+    @Test(expected = IllegalStateException.class)
+    public void this_should_end_with_illegal_state_exception() throws Exception {
+        HttpServletRequest request = newRequest();
+
+        when(request.isAsyncSupported()).thenReturn(false);
+
+        UploadParser.newAsyncParser(request).setupAsyncParse();
+    }
+}
